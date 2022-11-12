b3bb96411bc845fb0d94766870ee556789053c80
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2022-11-12 00:12:26 -0500

    Se elimina logo de la síntesis.

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -3,14 +3,7 @@
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    .row.mb-5.align-items-center
-      .col-lg-10
-        .titulo-sexto.color-acento-contenido.color-adicional-1.mb-5
-          h5 Gestión comunitaria del riesgo de desastres.
-          br
-          | Síntesis:  Diagnóstico para la gestión del riesgo de desastres.
-      .col-lg-2
-        img.img-a(src="@/assets/curso/temas/img_sin.png", alt="alt")
+    
 
     p.mb-5 A continuación, se presenta un mapa mental en donde podrá encontrar los contenidos trabajados en el componente formativo de manera gráfica. 
 
