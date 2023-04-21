be158bef1c92ddcd27a43d7994bdf9db630d4205
Camilo Bolaño <80546125+elrilacho@users.noreply.github.com>
2023-04-21 18:50:58 -0500

    Componente listo

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -23,7 +23,6 @@
     figure.mb-5(data-aos="zoom-in-down")
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/bAYEyhYrrGI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
     
 </template>
 
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -81,8 +81,7 @@
 
     figure.mb-5(data-aos="zoom-in-down")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/nS_6Z1WY1fU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     separador
     #t_2_2.titulo-segundo.color-acento-contenido.mt-5
